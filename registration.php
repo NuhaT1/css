348eb004c38fd4e08c0359c0ac6b10dcc2c64bae
NuhaT1 <nuhamin42@gmail.com>
2023-12-11 09:33:54 +0300

    some modification

--- registration.php
+++ registration.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Check if form data is present
     if (!empty($_POST['first_name'])) {
         // Retrieve form data
+        $ID = mysqli_real_escape_string($conn, $_POST['ID']);
         $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
         $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
         $birth_date = mysqli_real_escape_string($conn, $_POST['birth_date']);
@@ -15,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $school_name = mysqli_real_escape_string($conn, $_POST['school_name']);
 
         // SQL query to insert data
-        $sql = "INSERT INTO registration (ID,first_name, last_name, gender, grade, birth_date, school_name) VALUES ('$ID','$first_name', '$last_name', '$gender', '$grade', '$school_name', '$birth_date')";
+        $sql = "INSERT INTO registration (ID, first_name, last_name, gender, grade, birth_date, school_name) VALUES ('$ID','$first_name', '$last_name', '$gender', '$grade', '$school_name', '$birth_date')";
 
         if ($conn->query($sql) === TRUE) {
             echo "Registration successful!";
